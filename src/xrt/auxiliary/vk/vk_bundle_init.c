b4c5ffdecc416afd7c612bd28a2bddc5bb7d58ff
xantoz <bountyjedi@gmail.com>
2025-04-04 12:11:58 +0200

    Enable low latency boost on swapchain creation
    Yey! Low latency boostings.
    
    Also it should be rather interesting to modify monado to use this
    thing for pacing. The problem is that with how it's set up now the
    pacer is separate from the feedback from driver. But with
    VK_NV_low_latency2 these things kinda are not separate from each other
    I think.

--- src/xrt/auxiliary/vk/vk_bundle_init.c
+++ src/xrt/auxiliary/vk/vk_bundle_init.c
@@ -218,6 +218,7 @@ vk_fill_in_has_instance_extensions(struct vk_bundle *vk, struct u_string_list *e
 			continue;
 		}
 #endif // defined(VK_EXT_debug_utils)
+
 	}
 	// end of GENERATED instance extension code - do not modify - used by scripts
 }
@@ -763,6 +764,7 @@ fill_in_has_device_extensions(struct vk_bundle *vk, struct u_string_list *ext_li
 	vk->has_EXT_present_mode_fifo_latest_ready = false;
 	vk->has_ANDROID_external_format_resolve = false;
 	vk->has_GOOGLE_display_timing = false;
+	vk->has_NV_low_latency2 = false;
 
 	const char *const *exts = u_string_list_get_data(ext_list);
 	uint32_t ext_count = u_string_list_get_size(ext_list);
@@ -937,6 +939,14 @@ fill_in_has_device_extensions(struct vk_bundle *vk, struct u_string_list *ext_li
 			continue;
 		}
 #endif // defined(VK_GOOGLE_display_timing)
+
+#if defined(VK_NV_low_latency2)
+		if (strcmp(ext, VK_NV_LOW_LATENCY_2_EXTENSION_NAME) == 0) {
+			vk->has_NV_low_latency2 = true;
+			continue;
+		}
+#endif // defined(VK_NV_low_latency2)
+
 	}
 	// end of GENERATED device extension code - do not modify - used by scripts
 }
