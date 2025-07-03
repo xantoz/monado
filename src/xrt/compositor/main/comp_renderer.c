de7079ec9553a3fc3e717b5a7c6a4dd735999841
xantoz <bountyjedi@gmail.com>
2025-07-03 18:12:21 +0200

    do both waitforpresent and acquire swapchain image to test
    (still no actual debug prints lol)

--- src/xrt/compositor/main/comp_renderer.c
+++ src/xrt/compositor/main/comp_renderer.c
@@ -851,34 +851,34 @@ renderer_wait_for_present(struct comp_renderer *r, uint64_t desired_present_time
 		// returned will be handled quite soon by later calls
 		VkResult result = comp_target_wait_for_present(c->target, c->base.vk.present_id, timeout);
 		(void)result;
-	} else {
-		/*
-		 * For direct mode this makes us wait until the last frame has been
-		 * actually shown to the user, this avoids us missing that we have
-		 * missed a frame and miss-predicting the next frame.
-		 *
-		 * Not all drivers follow this behaviour, so KHR_present_wait
-		 * should be preferred in all circumstances.
-		 *
-		 * Only do this if we are ready.
-		 */
+	}
 
-		// Do the acquire
-		renderer_acquire_swapchain_image(r);
+	/*
+	 * For direct mode this makes us wait until the last frame has been
+	 * actually shown to the user, this avoids us missing that we have
+	 * missed a frame and miss-predicting the next frame.
+	 *
+	 * Not all drivers follow this behaviour, so KHR_present_wait
+	 * should be preferred in all circumstances.
+	 *
+	 * Only do this if we are ready.
+	 */
 
-		uint64_t after_ns = os_monotonic_get_ns();
+	// Do the acquire
+	renderer_acquire_swapchain_image(r);
 
-		// How long did it take?
-		/*
-		 * Make sure we at least waited 1ms before warning. Then check
-		 * if we are more then 1ms behind when we wanted to present.
-		 */
-		if (before_ns + U_TIME_1MS_IN_NS < after_ns && //
-		    desired_present_time_ns + U_TIME_1MS_IN_NS < after_ns) {
-			uint64_t diff_ns = after_ns - desired_present_time_ns;
-			double diff_ms_f = time_ns_to_ms_f(diff_ns);
-			COMP_WARN(c, "Compositor probably missed frame by %.2fms", diff_ms_f);
-		}
+	uint64_t after_ns = os_monotonic_get_ns();
+
+	// How long did it take?
+	/*
+	 * Make sure we at least waited 1ms before warning. Then check
+	 * if we are more then 1ms behind when we wanted to present.
+	 */
+	if (before_ns + U_TIME_1MS_IN_NS < after_ns && //
+	    desired_present_time_ns + U_TIME_1MS_IN_NS < after_ns) {
+		uint64_t diff_ns = after_ns - desired_present_time_ns;
+		double diff_ms_f = time_ns_to_ms_f(diff_ns);
+		COMP_WARN(c, "Compositor probably missed frame by %.2fms", diff_ms_f);
 	}
 }
 
